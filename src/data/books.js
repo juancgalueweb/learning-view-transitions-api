5a9ccede79683c927b3d7e746892e8d2b1346dfd
Juan Carlos Galue <juancgalue@icloud.com>
2023-07-05 14:21:19 -0400

    Primer commit

---
+++ src/data/books.js
@@ -0,0 +1,49 @@
+export const books = [
+  {
+    id: 'volando-sobre-el-asfalto',
+    title:
+      'Volando Sobre el Asfalto: Los Años que Tocamos la Gloria en la Fórmula 1',
+    img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/8d/45/8d4541bf7d33f90acadd75795fe3fb70.jpg',
+    opinion:
+      'Muy interesante el conocer las interioridades de los dos años en los que Fernando Alonso resultó ser campeón del mundo.',
+    author: 'Antonio Lobato'
+  },
+  {
+    id: 'el-hombre-que-invento-la-formula-1',
+    title:
+      'El Hombre que Invento la Formula 1: La Historia Secreta de Bernie Ecclestone',
+    img: 'https://images.cdn2.buscalibre.com/fit-in/360x360/85/ae/85aea62db8a0eda57a05ee7cc326b9c5.jpg',
+    opinion:
+      'Bernie Eccleston, el gran magnate de la Fórmula 1, es un tipo esquivo, misterioso, incluso. . . siniestro, en cuyas manos las carreras de coches han devenido un negocio de miles de millones. ¿Cómo llegó este vendedor de automóviles usados a convertirse en un multimillonario que viaja en jet privado y se codea con celebridades, héroes deportivos, presidentes de gobierno e incluso miembros de la realeza? Carreras amañadas, matrimonios de conveniencia, corrupción y vicio desfilan con nombre y apellido por las páginas de una biografía que no tiene nada que envidiar a la más jugosa novela negra.',
+    author: 'Tom Bower'
+  },
+  {
+    id: 'el-gran-circo-de-la-formula-1',
+    title: 'El Gran Circo de la Formula 1',
+    img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/fd/5f/fd5f64c64145e5c68841d2ef2f90a063.jpg',
+    opinion: '',
+    author: 'Nira Juanco'
+  },
+  {
+    id: 'kimi-raikkonen',
+    title: 'The Unknown Kimi Raikkonen (libro en Inglés)',
+    img: 'https://images.cdn1.buscalibre.com/fit-in/360x360/af/78/af783f3c2dcbd18e423d4c38a5e6c0d0.jpg',
+    opinion: '',
+    author: 'Kari Hotakainen'
+  },
+  {
+    id: 'jenson-button',
+    title:
+      'Jenson Button: Life to the Limit: My Autobiography (libro en Inglés)',
+    img: 'https://images.cdn3.buscalibre.com/fit-in/360x360/f9/39/f939903b75d534428b0a130bdee326aa.jpg',
+    opinion: '',
+    author: 'Jenson Button'
+  },
+  {
+    id: 'como-hacer-un-coche',
+    title: 'Como Hacer un Coche',
+    img: 'https://images.cdn3.buscalibre.com/fit-in/360x360/0c/cf/0ccfd518efc78291ead3b54bdabbbfa6.jpg',
+    opinion: '',
+    author: 'Adrian Newey'
+  }
+]
